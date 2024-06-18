a43333316853470dcacbf10966b76e36caa44dd9
VilayatKhan <mohdvilayatali.khan@arup.com>
2024-06-18 19:44:17 +0530

    Guessing game!

--- learning_rust/src/main.rs
+++ learning_rust/src/main.rs
@@ -1,3 +1,8 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter a number: ");
+    let mut str = String::new();
+    stdin().read_line(&mut str).expect("Error!");
+    println!("Number: {}", str);
 }
