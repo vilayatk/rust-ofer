24ad90011bf757d58e7605a1f99d084aa4ecf08c
VilayatKhan <mohdvilayatali.khan@arup.com>
2024-06-20 21:35:26 +0530

    Add comment

--- learning_rust/src/main.rs
+++ learning_rust/src/main.rs
@@ -2,7 +2,7 @@ use std::io::stdin;
 
 fn main() {
     println!("Enter a number: ");
-    let mut str = String::new();
+    let mut str = String::new(); // By default a variable is const
     stdin().read_line(&mut str).expect("Error!");
     println!("Number: {}", str);
 }
