11bdeada0e8159b506b2d7094065364b44a2c558
VilayatKhan <mohdvilayatali.khan@arup.com>
2024-06-20 21:50:50 +0530

    Constants vs Immutables vs Mutables - I

--- learning_rust/src/main.rs
+++ learning_rust/src/main.rs
@@ -1,8 +1,24 @@
-use std::io::stdin;
-
+//CONSTANTS and variables
 fn main() {
-    println!("Enter a number: ");
-    let mut str = String::new(); // By default a variable is const
-    stdin().read_line(&mut str).expect("Error!");
-    println!("Number: {}", str);
+    const PI: f64 = 3.141; // constant
+    let x: i32 = 20; // immutable, is a variable
+                     // x = x + 1; // error
+    let mut is_true: bool = true; // mutable
+    is_true = is_true && x % 2 == 0; // Allowed, since mutable
+
+    if is_true {
+        println!("pi is {}", PI);
+    } else {
+        println!("x is {}", x)
+    }
 }
+
+//use std::io::stdin;
+
+// fn main() {
+//     println!("Enter a number: ");
+//     let mut str = String::new(); // By default a variable is immutable
+//     stdin().read_line(&mut str).expect("Error!");
+//     println!("Number: {}", str);
+// }
+
