cd6b0511a3ccf728dca271ca78d171e1f562575b
Vilayat Khan <77726529+vilayatk@users.noreply.github.com>
2026-01-05 07:33:57 +0000

    borrowing

--- ownership/main.rs
+++ ownership/main.rs
@@ -10,7 +10,13 @@ fn main() {
 
     take_ownership(s3);
     //println!("s3 is no longer valid {s3}"); // Compiler error
+    let mut x = String::from("Something: ");
+    borrowing_and_updating(&mut x); // passing as reference and mutable.
 
+    println!("{x}");
+
+    borrow_and_return_object(&x);
+    println!("X still exists after the call. Borrowed and returned back.");
 }
 
 fn give_ownership() -> String {
@@ -20,6 +26,15 @@ fn give_ownership() -> String {
 
 fn take_ownership(x:String) {
    println!("Taken ownership {x}"); 
+}
+
+fn borrowing_and_updating(x : &mut String) {
+    x.push_str(". String is appended!");
+}
+
+fn borrow_and_return_object(x: &String) {
+    println!("Some usage of {x}");
+    println!("Returns the object back");
 }
 
 //fn dangle() -> &String {
